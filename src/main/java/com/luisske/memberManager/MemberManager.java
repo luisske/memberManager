85640b2293d3e9a4a35a0a5c851b8a9f25fa9072
luisske <luisske.dev@gmail.com>
2022-02-11 10:01:57 +0100

    activate method

--- src/main/java/com/luisske/memberManager/MemberManager.java
+++ src/main/java/com/luisske/memberManager/MemberManager.java
@@ -1,7 +1,18 @@
 package com.luisske.memberManager;
 
+import com.luisske.memberManager.service.AnalyzationService;
+import com.luisske.memberManager.service.CalendarService;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class MemberManager {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        AnalyzationService analyzationService = new AnalyzationService();
+        CalendarService calendarService = new CalendarService();
+
+        analyzationService.readInputFile();
+        calendarService.getUpcomingEvents();
     }
 }
